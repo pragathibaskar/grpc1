a2724b28877cce6fa9b2b495e87a9804a002f3ac
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-30 14:36:17 +0530

    Update ProvisionServiceImpl.java

--- src/main/java/com/cg/contService/grpc/ProvisionServiceImpl.java
+++ src/main/java/com/cg/contService/grpc/ProvisionServiceImpl.java
@@ -7,11 +7,18 @@ import io.grpc.stub.StreamObserver;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
+@RequestMapping("adif")
 @GRpcService
 public class ProvisionServiceImpl extends ProvisionServiceImplBase {
+	
+	public static String message;
 	
 public void creation(request req,StreamObserver<response> res) {
 		
@@ -23,10 +30,17 @@ public void creation(request req,StreamObserver<response> res) {
 		String msg = "Provisión creada para el expediente "+codigo+" en el periodo "+strDate;
 		System.out.println("message "+msg);
 		build.setMsg(msg);
+	        message = msg;
 		response b = build.build();
 		res.onNext(b);
 		res.onCompleted();
 		
+	}
+	
+	@CrossOrigin
+	@GetMapping("/grpc")
+	String getMsg() {
+		return message;
 	}
 
 }
