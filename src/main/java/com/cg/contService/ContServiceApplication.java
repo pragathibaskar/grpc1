250827e3051fcb9634da386cdb06de91ccc12923
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-10-12 12:48:29 +0530

    Update ContServiceApplication.java

--- src/main/java/com/cg/contService/ContServiceApplication.java
+++ src/main/java/com/cg/contService/ContServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan("com.cg.contService.*")
